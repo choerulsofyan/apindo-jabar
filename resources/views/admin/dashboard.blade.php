0c3ed58ab706d43e31e2835ce00b8756fce86094
Choerul Sofyan <choerulsofyanmf@gmail.com>
2025-01-05 12:10:28 +0700

    fix subheader

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,6 +2,13 @@
 
 @section('title', 'Dashboard')
 
+@section('subheader')
+    @include('partials.admin.subheader', [
+        'title' => 'Dashboard', // Dynamic title
+        'breadcrumbs' => [['name' => 'Home', 'url' => route('home')], ['name' => 'Dashboard', 'url' => '#']],
+    ])
+@endsection
+
 @section('content')
     <h1>Welcome to the Admin Dashboard</h1>
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -15,16 +15,8 @@
     <meta http-equiv="X-XSS-Protection" content="1; mode=block">
     <meta http-equiv="Strict-Transport-Security" content="max-age=31536000; includeSubDomains">
     <!--end::Primary Meta Tags-->
-    <!--begin::Fonts-->
-    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fontsource/source-sans-3@5.0.12/index.css" integrity="sha256-tXJfXfp6Ewt1ilPzLDtQnJV4hclT9XuaZUKyUvmyr+Q=" crossorigin="anonymous" /> --}}
-    <!--end::Fonts-->
 
-    <!--begin::Third Party Plugin(Bootstrap Icons)-->
-    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" integrity="sha256-9kPW/n5nn53j4WMRYAxe9c1rCY96Oogo/MKSVdKzPmI=" crossorigin="anonymous" /> --}}
-    <!--end::Third Party Plugin(Bootstrap Icons)-->
-    <!--begin::Required Plugin(AdminLTE)-->
     @vite(['resources/js/app.js', 'resources/sass/app.scss'])
-    <!--end::Required Plugin(AdminLTE)-->
 </head>
 <!--end::Head-->
 <!--begin::Body-->
@@ -47,13 +39,7 @@
                 <!--begin::Container-->
                 <div class="container-fluid">
                     <!--begin::Breadcrumb-->
-                    @include('partials.admin.subheader', [
-                        'title' => 'Dashboard', // Dynamic page title
-                        'breadcrumbs' => [
-                            ['name' => 'Home', 'url' => '#'],
-                            ['name' => 'Fixed Layout', 'url' => '#'],
-                        ],
-                    ])
+                    @yield('subheader')
                     <!--end::Breadcrumb-->
                 </div>
                 <!--end::Container-->
@@ -63,7 +49,6 @@
             <div class="app-content">
                 <!--begin::Container-->
                 <div class="container-fluid">
-                    {{-- should this part placed in a separated partial file? --}}
                     <!--begin::Content-->
                     @yield('content')
                     <!--end::Content-->
@@ -78,18 +63,7 @@
         <!--end::Footer-->
     </div>
     <!--end::App Wrapper-->
-    {{-- should this part placed in a separated partial file? --}}
     <!--begin::Script-->
-    <!--begin::Third Party Plugin(OverlayScrollbars)-->
-    {{-- <script src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/browser/overlayscrollbars.browser.es6.min.js" integrity="sha256-dghWARbRe2eLlIJ56wNB+b760ywulqK3DzZYEpsg2fQ=" crossorigin="anonymous"></script> --}}
-    <!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Required Plugin(popperjs for Bootstrap 5)-->
-    {{-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"> </script> --}}
-    <!--end::Required Plugin(popperjs for Bootstrap 5)--><!--begin::Required Plugin(Bootstrap 5)-->
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"> </script> --}}
-    <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
-    {{-- <script src="../../../dist/js/adminlte.js"></script> --}}
-    <!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
-
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             const sidebarWrapper = document.querySelector('.sidebar-wrapper');
@@ -104,7 +78,6 @@
             }
         });
     </script>
-    <!--end::OverlayScrollbars Configure-->
     @stack('scripts')
     <!--end::Script-->
 </body>
