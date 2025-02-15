e462c1095322fbd3341e8c5cab86b5a20021eb6e
Choerul Sofyan <choerulsofyanmf@gmail.com>
2025-02-15 15:34:58 +0700

    fix news section ui

--- resources/views/public/partials/news.blade.php
+++ resources/views/public/partials/news.blade.php
@@ -3,7 +3,7 @@
         <h2 class="text-center fw-bolder mb-4 text-primary">BERITA</h2>
         @if ($latestNews->isEmpty())
             <div class="alert alert-info">
-                No news available.
+                Tidak ada berita.
             </div>
         @else
             <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
@@ -20,15 +20,19 @@
                                 @endif
                             </div>
                             <div class="card-body d-flex flex-column">
-                                <p class="card-text mt-0 mb-0">
-                                    <i class="bi bi-geo-alt-fill me-1"></i>
-                                    <small class="text-muted">{{ $news->place }}</small>
-                                    <i class="bi bi-calendar ms-3 me-1"></i>
-                                    <small class="text-muted">{{ $news->formatted_date }}</small>
-                                </p>
+                                <div class="card-text mt-0 mb-0">
+                                    @if ($news->place)
+                                        <i class="bi bi-geo-alt-fill me-1"></i>
+                                        <small class="text-muted">{{ $news->place }}</small>
+                                    @endif
+                                    @if ($news->formatted_date)
+                                        <i class="bi bi-calendar{{ $news->place ? ' ms-3' : '' }} me-1"></i>
+                                        <small class="text-muted">{{ $news->formatted_date }}</small>
+                                    @endif
+                                </div>
                                 <hr>
                                 <h5 class="card-title fw-bold">{{ $news->title }}</h5>
-                                <p class="card-text flex-grow-1">{{ $news->short_content }}</p>
+                                <div class="card-text flex-grow-1">{!! $news->short_content !!}</div>
                                 <a href="{{ route('news.detail', $news->id) }}"
                                     class="btn btn-outline-primary stretched-link mt-auto">
                                     Baca Selengkapnya
@@ -38,7 +42,7 @@
                     </div>
                 @endforeach
             </div>
-            <div class="text-center mt-5">
+            <div class="text-start mt-5">
                 <h5>
                     <a href="{{ route('home') }}" class="text-decoration-none">
                         Lihat Semua Berita
