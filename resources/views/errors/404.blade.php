d1906674b3c136e9957b1897b3ec0a573af02f59
Choerul Sofyan <choerulsofyanmf@gmail.com>
2025-02-15 15:31:41 +0700

    fix 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,11 +3,11 @@
 @section('title', '404 Not Found')
 
 @section('content')
-    <div class="container text-center py-5">
+    <div class="container text-center py-5 vh-100">
         <h1 class="display-1 fw-bold text-primary">404</h1>
-        <p class="lead">Oops! The page you're looking for could not be found.</p>
+        <p class="lead">Oops! Halaman yang Anda cari tidak ditemukan.</p>
         <p>
-            <a href="{{ route('home') }}" class="btn btn-primary">Go Back to Homepage</a>
+            <a href="{{ route('home') }}" class="text-decoration-none">Kembali ke Beranda</a>
         </p>
     </div>
 @endsection
