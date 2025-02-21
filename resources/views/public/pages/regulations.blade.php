d2c3f96e7bb0ebe3f8ade2e3ad3eeff51ab8e968
Choerul Sofyan <choerulsofyanmf@gmail.com>
2025-02-21 19:58:08 +0700

    fix regulation page in public

--- resources/views/public/pages/regulations.blade.php
+++ resources/views/public/pages/regulations.blade.php
@@ -12,9 +12,9 @@
                 <div class="col-md-6">
                     <form action="{{ route('regulations') }}" method="GET">
                         <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Search by title..." name="search"
-                                value="{{ request('search') }}">
-                            <button class="btn btn-outline-secondary" type="submit">
+                            <input type="text" class="form-control" placeholder="Cari regulasi dan tekan enter..."
+                                name="search" value="{{ request('search') }}">
+                            <button class="btn btn-secondary" style="border: solid 1px #dee2e6;" type="submit">
                                 <i class="bi bi-search"></i> Search
                             </button>
                         </div>
@@ -28,14 +28,14 @@
                 </div>
             @else
                 <div class="table-responsive">
-                    <table class="table table-striped table-hover">
+                    <table class="table table-striped table-hover table-bordered">
                         <thead>
                             <tr>
-                                <th>No</th>
-                                <th>
+                                <th>No.</th>
+                                <th class="w-75">
                                     <a href="{{ route('regulations', ['sort_by' => 'title', 'sort_order' => request('sort_by') == 'title' && request('sort_order') == 'asc' ? 'desc' : 'asc', 'search' => request('search')]) }}"
                                         class="text-decoration-none link-dark">
-                                        Title
+                                        Nama
                                         @if (request('sort_by') == 'title')
                                             <i
                                                 class="bi bi-sort-{{ request('sort_order', 'asc') == 'asc' ? 'up' : 'down' }}"></i>
@@ -45,7 +45,7 @@
                                 <th>
                                     <a href="{{ route('regulations', ['sort_by' => 'date', 'sort_order' => request('sort_by') == 'date' && request('sort_order') == 'asc' ? 'desc' : 'asc', 'search' => request('search')]) }}"
                                         class="text-decoration-none link-dark">
-                                        Date
+                                        Tanggal
                                         @if (request('sort_by') == 'date')
                                             <i
                                                 class="bi bi-sort-{{ request('sort_order', 'asc') == 'asc' ? 'up' : 'down' }}"></i>
@@ -60,8 +60,7 @@
                                 <tr>
                                     <td>{{ ++$i }}</td>
                                     <td>{{ $regulation->title }}</td>
-                                    {{-- <td>{{ $regulation->date->format('F j, Y') }}</td> --}}
-                                    <td></td>
+                                    <td>{{ $regulation->date }}</td>
                                     <td>
                                         @if ($regulation->file)
                                             <a href="{{ Storage::url('public/regulations/' . $regulation->file) }}"
@@ -80,8 +79,37 @@
                 </div>
 
                 {{-- Pagination Links --}}
-                <div class="d-flex justify-content-center">
-                    {{ $regulations->appends(['search' => request('search'), 'sort_by' => request('sort_by'), 'sort_order' => request('sort_order')])->links() }}
+                <div class="d-flex justify-content-between">
+                    <div class="text-muted">
+                        Showing {{ $regulations->firstItem() }} to {{ $regulations->lastItem() }} of
+                        {{ $regulations->total() }} results
+                    </div>
+                    @if ($regulations->hasPages())
+                        <div>
+                            <ul class="pagination pagination-sm m-0 float-end">
+                                @if ($regulations->onFirstPage())
+                                    <li class="page-item disabled"><span class="page-link">&laquo;</span></li>
+                                @else
+                                    <li class="page-item"><a class="page-link"
+                                            href="{{ $regulations->previousPageUrl() }}">&laquo;</a></li>
+                                @endif
+
+                                @foreach ($regulations->getUrlRange(1, $regulations->lastPage()) as $page => $url)
+                                    <li class="page-item {{ $regulations->currentPage() == $page ? 'active' : '' }}">
+                                        <a class="page-link" href="{{ $url }}">{{ $page }}</a>
+                                    </li>
+                                @endforeach
+
+                                @if ($regulations->hasMorePages())
+                                    <li class="page-item"><a class="page-link"
+                                            href="{{ $regulations->nextPageUrl() }}">&raquo;</a>
+                                    </li>
+                                @else
+                                    <li class="page-item disabled"><span class="page-link">&raquo;</span></li>
+                                @endif
+                            </ul>
+                        </div>
+                    @endif
                 </div>
             @endif
         </div>
